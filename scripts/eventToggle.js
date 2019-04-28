c0142f6add5529aae1766c291d21248619d55a1e
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-04-28 22:22:50 +0100

    done testominals-events

--- scripts/eventToggle.js
+++ scripts/eventToggle.js
@@ -1,5 +1,46 @@
-$('.event-read-btn').on('click',function(){
-//    console.log($(this).parents().next('.event-detail').html())
-$(this).parents().next('.event-detail').toggleClass('expand');
-$(this).find('.fas').toggleClass('rotate');
-})
+const xhr = new XMLHttpRequest();
+const eventList = document.getElementById("event-list");
+xhr.open("GET", "./api/eventsData.json", true);
+xhr.responseType = "text";
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    var eventData = JSON.parse(xhr.responseText);
+    // console.log(eventData);
+    for (let i = 0; i < eventData.length; i++) {
+      // console.log(eventData[i]);
+      eventList.innerHTML += eventTemp(eventData[i]);
+    }
+  } else {
+    console.log("Error", xhr.statusText);
+  }
+};
+xhr.send();
+
+const eventTemp = obj => {
+  return `
+  <div class="event-container">
+  <ul class="event-date">
+    <li>${obj.eventDate}</li>
+    <li>${obj.eventMonth}</li>
+  </ul>
+  <h2 class="event-name">
+    ${obj.eventName}
+    <i class="event-read-btn fas fa-caret-up"></i>
+  </h2>
+  <div class="event-detail">
+    <div>
+      ${obj.eventInfo}
+    </div>
+  </div>
+</div>
+  `;
+};
+$(document).ready(() => {
+  $(".event-read-btn").on("click", function() {
+    $(this)
+      .parents()
+      .next(".event-detail")
+      .toggleClass("expand");
+    $(this).toggleClass("rotate");
+  });
+});
