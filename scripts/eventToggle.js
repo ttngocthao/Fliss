c35c1d0f25397c2ae667602a6c85253ac9e7c6c3
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-05-20 22:08:30 +0100

    commit

--- scripts/eventToggle.js
+++ scripts/eventToggle.js
@@ -1,47 +1,47 @@
-const xhr = new XMLHttpRequest();
-const eventList = document.getElementById("event-list");
-xhr.open("GET", "./api/eventsData.json", true);
-xhr.responseType = "text";
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    var eventData = JSON.parse(xhr.responseText);
-    // console.log(eventData);
-    for (let i = 0; i < eventData.length; i++) {
-      // console.log(eventData[i]);
-      if (eventList !== null) {
-        eventList.innerHTML += eventTemp(eventData[i]);
-      }
-    }
-  } else {
-    console.log("Error", xhr.statusText);
-  }
-};
-xhr.send();
+// const xhr = new XMLHttpRequest();
+// const eventList = document.getElementById("event-list");
+// xhr.open("GET", "./api/eventsData.json", true);
+// xhr.responseType = "text";
+// xhr.onload = function() {
+//   if (xhr.status === 200) {
+//     var eventData = JSON.parse(xhr.responseText);
+//     // console.log(eventData);
+//     for (let i = 0; i < eventData.length; i++) {
+//       // console.log(eventData[i]);
+//       if (eventList !== null) {
+//         eventList.innerHTML += eventTemp(eventData[i]);
+//       }
+//     }
+//   } else {
+//     console.log("Error", xhr.statusText);
+//   }
+// };
+// xhr.send();
 
-const eventTemp = obj => {
-  return `
-  <div class="event-container">
-  <ul class="event-date">
-    <li>${obj.eventDate}</li>
-    <li>${obj.eventMonth}</li>
-  </ul>
-  <h2 class="event-name">
-    ${obj.eventName}
-  </h2>
-  <i class="event-read-btn fas fa-caret-up"></i>
-  <div class="event-detail">
-    <div>
-      ${obj.eventInfo}
-    </div>
-  </div>
-</div>
-  `;
-};
-$(document).ready(() => {
-  $(".event-read-btn").on("click", function() {
-    $(this)
-      .next(".event-detail")
-      .toggleClass("expand");
-    $(this).toggleClass("rotate");
-  });
-});
+// const eventTemp = obj => {
+//   return `
+//   <div class="event-container">
+//   <ul class="event-date">
+//     <li>${obj.eventDate}</li>
+//     <li>${obj.eventMonth}</li>
+//   </ul>
+//   <h2 class="event-name">
+//     ${obj.eventName}
+//   </h2>
+//   <i class="event-read-btn fas fa-caret-up"></i>
+//   <div class="event-detail">
+//     <div>
+//       ${obj.eventInfo}
+//     </div>
+//   </div>
+// </div>
+//   `;
+// };
+// $(document).ready(() => {
+//   $(".event-read-btn").on("click", function() {
+//     $(this)
+//       .next(".event-detail")
+//       .toggleClass("expand");
+//     $(this).toggleClass("rotate");
+//   });
+// });
--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,33 +1,33 @@
-const xhr = new XMLHttpRequest();
-const testimonialsList = document.getElementById("testimonials-list");
-xhr.open("GET", "./api/testimonialsData.json", true);
-xhr.responseType = "text";
+// const xhr = new XMLHttpRequest();
+// const testimonialsList = document.getElementById("testimonials-list");
+// xhr.open("GET", "./api/testimonialsData.json", true);
+// xhr.responseType = "text";
 
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    var reviewData = JSON.parse(xhr.responseText);
-    // console.log(reviewData);
-    for (let i = 0; i < reviewData.length; i++) {
-      // console.log(reviewData[i]);
-      testimonialsList.innerHTML += reviewTemp(reviewData[i]);
-    }
-  } else {
-    console.log("Error", xhr.statusText);
-  }
-};
-xhr.send();
+// xhr.onload = function() {
+//   if (xhr.status === 200) {
+//     var reviewData = JSON.parse(xhr.responseText);
+//     // console.log(reviewData);
+//     for (let i = 0; i < reviewData.length; i++) {
+//       // console.log(reviewData[i]);
+//       testimonialsList.innerHTML += reviewTemp(reviewData[i]);
+//     }
+//   } else {
+//     console.log("Error", xhr.statusText);
+//   }
+// };
+// xhr.send();
 
-//template for each review
-const reviewTemp = obj => {
-  return `
-        <div class='review-wrapper'>
-            <div class='reviewer-info'>                
-                <figure class='reviewer-img'>
-                    <img src='${obj.profileImgUrl}'>
-                </figure>
-                <h2 class='reviewer-name'>${obj.name}</h2>
-            </div>
-            <div class='review-msg'>${obj.review}</div>
-        </div>
-    `;
-};
+// //template for each review
+// const reviewTemp = obj => {
+//   return `
+//         <div class='review-wrapper'>
+//             <div class='reviewer-info'>
+//                 <figure class='reviewer-img'>
+//                     <img src='${obj.profileImgUrl}'>
+//                 </figure>
+//                 <h2 class='reviewer-name'>${obj.name}</h2>
+//             </div>
+//             <div class='review-msg'>${obj.review}</div>
+//         </div>
+//     `;
+// };
