0ecee3d6f283b82f5ce44c09335ff888d79d1971
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-05-21 22:08:21 +0100

    commit

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,33 +1,33 @@
-// const xhr = new XMLHttpRequest();
-// const testimonialsList = document.getElementById("testimonials-list");
-// xhr.open("GET", "./api/testimonialsData.json", true);
-// xhr.responseType = "text";
+const reviewsXhr = new XMLHttpRequest();
+const testimonialsList = document.getElementById("testimonials-list");
+reviewsXhr.open("GET", "./api/testimonialsData.json", true);
+reviewsXhr.responseType = "text";
 
-// xhr.onload = function() {
-//   if (xhr.status === 200) {
-//     var reviewData = JSON.parse(xhr.responseText);
-//     // console.log(reviewData);
-//     for (let i = 0; i < reviewData.length; i++) {
-//       // console.log(reviewData[i]);
-//       testimonialsList.innerHTML += reviewTemp(reviewData[i]);
-//     }
-//   } else {
-//     console.log("Error", xhr.statusText);
-//   }
-// };
-// xhr.send();
+reviewsXhr.onload = function() {
+  if (reviewsXhr.status === 200) {
+    var reviewData = JSON.parse(reviewsXhr.responseText);
+    // console.log(reviewData);
+    for (let i = 0; i < reviewData.length; i++) {
+      // console.log(reviewData[i]);
+      testimonialsList.innerHTML += reviewTemp(reviewData[i]);
+    }
+  } else {
+    console.log("Error", reviewsXhr.statusText);
+  }
+};
+reviewsXhr.send();
 
-// //template for each review
-// const reviewTemp = obj => {
-//   return `
-//         <div class='review-wrapper'>
-//             <div class='reviewer-info'>
-//                 <figure class='reviewer-img'>
-//                     <img src='${obj.profileImgUrl}'>
-//                 </figure>
-//                 <h2 class='reviewer-name'>${obj.name}</h2>
-//             </div>
-//             <div class='review-msg'>${obj.review}</div>
-//         </div>
-//     `;
-// };
+//template for each review
+const reviewTemp = obj => {
+  return `
+        <div class='review-wrapper'>
+            <div class='reviewer-info'>
+                <figure class='reviewer-img'>
+                    <img src='${obj.profileImgUrl}'>
+                </figure>
+                <h2 class='reviewer-name'>${obj.name}</h2>
+            </div>
+            <div class='review-msg'>${obj.review}</div>
+        </div>
+    `;
+};
